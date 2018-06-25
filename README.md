da2943bbdb591f12da98edb6626222491d477b3d
admin <qinkangwu@xs1h.com>
2018-06-25 15:17:46 +0800

    asd

---
+++ README.md
@@ -0,0 +1,11 @@
+## Description:
+> 统计文件夹里的文件总行数
+
+## Installation:
+```
+npm i -g file-lines
+```
+## Usage:
+```
+file-lines --cwd '/Users/admin/.....' [--ignore 'images,build,dist,.git,node_modules,.....']
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+var argv = require('optimist').argv;
+var fs = require('fs');
+var cwd,ignoreArr,totalLines = 0;
+if(!argv.cwd)throw Error('cwd参数为空');
+cwd = argv.cwd;
+argv.ignore && (ignoreArr = argv.ignore.split(','));
+var arr = diffArray(fs.readdirSync(cwd),ignoreArr);
+arr.forEach((res)=>{
+    fileLines(cwd + '/' + res);
+})
+console.log(`${cwd}目录下指定的文件行数为${totalLines}`);
+function fileLines(path){
+    var stat = fs.statSync(path);
+    if(stat.isDirectory()){
+        var arr2 =diffArray(fs.readdirSync(path),ignoreArr);
+        arr2.forEach((res)=>{
+            fileLines(path + '/' + res);
+        })
+    }else{
+        totalLines += fs.readFileSync(path).toString().split('\n').length;
+    }
+}
+function diffArray(one, two) {
+    if (!Array.isArray(two)) {
+      return one.slice();
+    }
+  
+    var tlen = two.length
+    var olen = one.length;
+    var idx = -1;
+    var arr = [];
+  
+    while (++idx < olen) {
+      var ele = one[idx];
+  
+      var hasEle = false;
+      for (var i = 0; i < tlen; i++) {
+        var val = two[i];
+  
+        if (ele === val) {
+          hasEle = true;
+          break;
+        }
+      }
+  
+      if (hasEle === false) {
+        arr.push(ele);
+      }
+    }
+    return arr;
+}
